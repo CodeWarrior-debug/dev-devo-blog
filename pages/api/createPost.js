221a71211fb3dd2ce913148cdcbce23adae5867b
James Jordan <jpjordan1987@gmail.com>
2023-03-27 10:31:39 -0400

    trying ot get this to work with req.params

--- pages/api/createPost.js
+++ pages/api/createPost.js
@@ -1,22 +1,24 @@
 // import React, { useEffect } from "react";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // import { getCountFromServer,getDocs,getDoc,getFirestore ,collection, addDoc,doc, setDoc, Timestamp, updateDoc, arrayUnion, arrayRemove, increment, deleteDoc} from "firebase/firestore";
-import { setDoc, getFirestore, doc } from "firebase/firestore";
+import { setDoc, doc } from "firebase/firestore";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBXjSO1dKSAjVYVmcNURaRdpPueZRXoTdU",
-  authDomain: "dev-blog-b9b09.firebaseapp.com",
-  projectId: "dev-blog-b9b09",
-  storageBucket: "dev-blog-b9b09.appspot.com",
-  messagingSenderId: "452713926371",
-  appId: "1:452713926371:web:67a68e3b801903c133bfd6",
-  measurementId: "G-KBC14G3NMK",
-};
+import {db} from "../../lib/firesStoreRef"
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBXjSO1dKSAjVYVmcNURaRdpPueZRXoTdU",
+//   authDomain: "dev-blog-b9b09.firebaseapp.com",
+//   projectId: "dev-blog-b9b09",
+//   storageBucket: "dev-blog-b9b09.appspot.com",
+//   messagingSenderId: "452713926371",
+//   appId: "1:452713926371:web:67a68e3b801903c133bfd6",
+//   measurementId: "G-KBC14G3NMK",
+// };
   
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 
 export default function handler(req,res){
@@ -34,17 +36,17 @@ export default function handler(req,res){
       // constructor(title,post,author,createddateTime, updateddateTime,subtitle,url,idNum,tagsArr,commentsArr){
 
          
-      await setDoc(doc(db, "posts","3"), {
-        title: "The best topics",
-        post: "You know what they are! God and good books!",
-        author: "James",
-        createddateTime: "99/99/12 02:02",
-        updateddateTime: new Date(),
-        subtitle: "You can't wait to hear just how good these topics are...",
-        url: "/the_best_topics_2",
-        idNum: 2,
-        tagsArr: ["theology", "literature"],
-        commentsArr: ["great God almighty", "i hate emerson"],
+      await setDoc(doc(db, "posts","4"), {
+        title: req.params.title,
+        post: req.params.post
+        // author: "James",
+        // createddateTime: "99/99/12 02:02",
+        // updateddateTime: new Date(),
+        // subtitle: "You can't wait to hear just how good these topics are...",
+        // url: "/the_best_topics_2",
+        // idNum: 2,
+        // tagsArr: ["theology", "literature"],
+        // commentsArr: ["great God almighty", "i hate emerson"],
       });
   
   
@@ -66,7 +68,7 @@ export default function handler(req,res){
 
     
     
-    res.status(200).json({status:'happy it worked!'})
+    res.status(200).json("title through",req.params.title)
 
       
   
--- pages/testerAPI.js
+++ pages/testerAPI.js
@@ -1,11 +1,24 @@
 import React, { useEffect } from "react";
 
 
+const title= "The best topics";
+const post= "You know what they are! God and good books!";
+const author= "James";
+const createddateTime= "99/99/12 02=02";
+const updateddateTime= new Date();
+const   subtitle= "You can't wait to hear just how good these topics are...";
+const url= "/the_best_topics_2";
+const idNum= 2;
+const tagsArr= ["theology", "literature"];
+const commentsArr= ["great God almighty", "i hate emerson"];
+
 
 const TestPageAPI = () => {
   // const coll = collection(db, "cities");
   // const snapshot = await getCountFromServer(coll);
   // console.log('count: ', snapshot.data().count);
+
+
   
   const getData = async ()=>{
      const response = await fetch('api/createPost')
