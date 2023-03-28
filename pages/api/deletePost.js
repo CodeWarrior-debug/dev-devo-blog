0bd70a7caa207a3fcf2c93130b687fc4e79187f7
James Jordan <jpjordan1987@gmail.com>
2023-03-27 22:48:48 -0400

    delete working

--- pages/api/deletePost.js
+++ pages/api/deletePost.js
@@ -1,21 +1,15 @@
 // import { getCountFromServer,getDocs,getDoc,getFirestore ,collection, addDoc,doc, setDoc, Timestamp, updateDoc, arrayUnion, arrayRemove, increment, deleteDoc} from "firebase/firestore";
-import { collection, doc } from "firebase/firestore";
+import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../../lib/firesStoreRef";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   try {
 
-    // const docRef = doc(db, "posts", req.headers.custompostindex.toString());
+    await deleteDoc(doc(db,"posts",req.headers.custompostindex.toString()))
 
-
-      db.collection("posts").doc(req.headers.custompostindex.toString()).delete.then(()=>
-      {
-        console.log("# ",req.headers.custompostindex.toString() ," doc successfully deleted")
-      })
-
-    
+    console.log("doc deleted")
 
 
   } catch (err) {
