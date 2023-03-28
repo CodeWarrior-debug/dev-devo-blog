b0d0afd5d2e4cf8f75cdfa1288cae4d26cec46d9
James Jordan <jpjordan1987@gmail.com>
2023-03-27 22:00:03 -0400

    now posting

--- pages/testerreadPost.js
+++ pages/testerreadPost.js
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from "react";
 
-
-
 // https://www.youtube.com/watch?v=GgzWFxIiwK4&list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH&index=42
 
 // TODO research error ---- API resolved without sending a response for /api/readPost, this may result in stalled requests.
 
-
 const TestPageAPI = () => {
+  const [post, setPost] = useState("");
 
-  const [post, setPost] = useState('');
-  const sampleValue = 1;
-  
-
+  const number = 2;
 
   // const coll = collection(db, "cities");
   // const snapshot = await getCountFromServer(coll);
   // console.log('count: ', snapshot.data().count);
-  
-  const getData = async ()=>{
-     const response = await fetch('api/readPost', sampleValue)
-     const data = await response.json()
-     setPost(data)
+
+  const getData = async () => {
+    const response = await fetch("api/readPost", {
+      headers: {
+        customPostIndex: number.toString(),
+      },
+    });
+    const data = await response.json();
+    setPost(data);
     //  console.log(data)
-  }
-  
-  
+  };
+
   useEffect(() => {
-
-
-    getData()
+    getData();
   }, []);
 
-  return <>
-  <p>{post.post}</p>
-  </>;
+  return (
+    <>
+      <p>{post.author}</p>
+    </>
+  );
 };
 
 export default TestPageAPI;
