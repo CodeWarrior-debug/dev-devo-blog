d8bae4c63fb6f9714dbf5bfcabcc91a84461c25c
James Jordan <jpjordan1987@gmail.com>
2023-04-05 13:31:58 -0400

    cleanup on update

--- pages/api/updatePost.js
+++ pages/api/updatePost.js
@@ -1,68 +1,18 @@
-// import { getCountFromServer,getDocs,getDoc,getFirestore ,collection, addDoc,doc, setDoc, Timestamp, updateDoc, arrayUnion, arrayRemove, increment, deleteDoc} from "firebase/firestore";
-import { setDoc, doc } from "firebase/firestore";
-import { db } from "../../lib/firesStoreRef";
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  
-  
-  try {
-    const title = req.body.title;
-    const postBody = req.body.postBody;
-    const subtitle = req.body.subtitle;
-    const updateddateTime = req.body.updateddateTime;
-    // const author = req.body.author;
-    // const createddateTime = req.body.createddateTime;
-    // const url = slugify(req.body.title).toLowerCase();
-    const idNum = req.body.idNum;
-    // const tagsArr = req.body.tagsArr;
-    // const commentsArr = req.body.commentsArr;
-
-    const updatedPost = {
-      title: title,
-      postBody: postBody,
-      updateddateTime: updateddateTime,
-      subtitle: subtitle,
-      // author: author,
-      // createddateTime: createddateTime,
-      // url: url + "-" + idNum,
-      idNum: idNum,
-      // tagsArr: tagsArr,
-      // commentsArr: commentsArr,
-    };
-
-    const updatePost = async () => {
-      
-
-      // await setDoc(doc(db, "posts", idNum.toString()), newPost);
-
-   const postRef = doc(db,"posts",updatedPost.idNum.toString())
+import {doc, updateDoc} from "firebase/firestore"
+import { db } from "@/lib/firesStoreRef"
 
 
-     setDoc(postRef,{title: "the very very best things"},{merge : true})
-// {postBody: updatedPost.postBody},{updateddateTime: updatedPost.updateddateTime},{subtitle: updatedPost.subtitle}
-      // const refPostBody = 
-      // const refUpdatedTime = 
-      // const refSubtitle = 
+export default async function handler(req, res) {
 
+    const postRef = doc(db,"posts",req.headers.custompostindex)
 
-
-      // const refTitle = newPost.title
-      // const refPostBody = newPost.title
-      // const refUpdatedTime = newPost.title
-      // const refSubtitle = newPost.title
-
-      // setDoc(refTitle, {title: true}, {merge:true})
-      // setDoc(refPostBody, {postBody : true}, {merge:true})
-      // setDoc(refUpdatedTime, {updateddateTime: true}, {merge:true})
-      // setDoc(refSubtitle, {subtitle: true}, {merge:true})
-
-
-    };
-
-    updatePost();
-  } catch (err) {
-    console.log(err);
+    await updateDoc(postRef, { title: req.body.title });
+    await updateDoc(postRef, { postBody:req.body.postBody });
+    await updateDoc(postRef, { subtitle:req.body.subtitle });
+    await updateDoc(postRef, { updateddateTime: new Date().toDateString(), });
+    
+    
+    // subtitle:req.body.subtitle,
+    // updateddateTime: new Date().toDateString(),
+    res.status(200).json({ message: 'post successful' })
   }
-}
--- pages/blogs/[blog].js
+++ pages/blogs/[blog].js
@@ -7,8 +7,7 @@ import "react-quill/dist/quill.snow.css";
 import { useEffect } from "react";
 import { db } from "@/lib/firesStoreRef";
 
-import {useRouter} from "next/router"
-
+import { useRouter } from "next/router";
 
 // title,post,author,date,subtitle,url,idNum,tagsArr,commentsArr
 
@@ -18,98 +17,85 @@ const QuillNoSSRWrapper = dynamic(import("react-quill"), {
 });
 
 export default function Blog() {
+  // states
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("ANONYMOUS");
+  const [subtitle, setSubtitle] = useState("");
+  const [createddateTime, setCreatedDateTime] = useState("");
+  const [updateddateTime, setUpdatedDateTime] = useState("");
+  const [url, setUrl] = useState("");
 
-      // states 
-      const [title, setTitle] = useState("");
-      const [idNum,setIdNum]=useState("")
-      const [content, setContent] = useState("");
-      const [author, setAuthor] = useState("ANONYMOUS")
-      const [subtitle, setSubtitle] =useState("")
-      const [createddateTime, setCreatedDateTime]=useState("")
-      const [updateddateTime,setUpdatedDateTime]=useState("")
+  const router = useRouter();
 
-    
-    const router = useRouter()
-  
-    const getData = async () => {
-      const response = await fetch("../api/readPost", {
-        headers: {
-          customPostIndex: router.query.blog,
-        
-        },
-      });
-      
-      const data = await response.json()
-      // .then(
-
-      // )
-      // .catch(console.log(err))
-    //   setPost(data);
-      // console.log(data)
-      setTitle(data.title),
-      setContent(data.postBody)
-
-    };
-
-  useEffect(()=>{
-    getData();
-    
-    
-  })
-
-      const params = {
-      title:title,
-      postBody:content,
-      // author:author,
-      subtitle:subtitle,
-      // createddateTime:createddateTime,
-      updateddateTime:updateddateTime,
-      // url:url - added downstream
-
-    }
-
-    const options = {
-      method: "POST",
-      body: JSON.stringify(params),
+  const getData = async () => {
+    const response = await fetch("../api/readPost", {
       headers: {
-        "Content-Type": "application/json",
-        "custompostindex":idNum
+        customPostIndex: router.query.blog,
       },
-    };
+    });
 
+    const data = await response.json();
+    // .then(
 
+    // )
+    // .catch(console.log(err))
+    //   setPost(data);
+    // console.log(data)
+    setUrl(data.url);
+    setTitle(data.title), setContent(data.postBody);
+    setUpdatedDateTime(new Date().toDateString());
+  };
 
-  const updateData = async ()=>{
-    const response = await fetch("../api/updatePost2",options)
+  useEffect(() => {
+    getData();
+  });
+
+  const params = {
+    title: title,
+    postBody: content,
+    subtitle: subtitle,
+    updateddateTime: updateddateTime,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(params),
+    headers: {
+      "Content-Type": "application/json",
+      custompostindex: router.query.blog,
+    },
+  };
+
+  const updateData = async () => {
+    const response = await fetch("../api/updatePost", options);
     // const response = await fetch("../api/updatePost2")
-    const data= await response.json()
-    console.log(data)
+    const data = await response.json();
+    console.log(data);
+  };
 
+  // const [url,setUrl]=useState("")
+  // const [tagsArr,setTagsArr]=useState("")
 
-  }
+  // const params = {
+  //   title:title,
+  //   idNum:idNum,
+  //   postBody:content,
+  //   author:author,
+  //   subtitle:subtitle,
+  //   createddateTime:createddateTime,
+  //   updateddateTime:updateddateTime,
+  //   // url:url - added downstream
 
-    // const [url,setUrl]=useState("")
-    // const [tagsArr,setTagsArr]=useState("")
-    
-    // const params = {
-    //   title:title,
-    //   idNum:idNum,
-    //   postBody:content,
-    //   author:author,
-    //   subtitle:subtitle,
-    //   createddateTime:createddateTime,
-    //   updateddateTime:updateddateTime,
-    //   // url:url - added downstream
+  // }
 
-    // }
-
-    // const options = {
-    //   method: "POST",
-    //   body: JSON.stringify(params),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // };
+  // const options = {
+  //   method: "POST",
+  //   body: JSON.stringify(params),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // };
 
   function handleTitleChange(event) {
     event.preventDefault();
@@ -136,95 +122,79 @@ export default function Blog() {
     },
   };
 
-//   const makeQuery = async () => {
-//     const response = await fetch("api/createPost", options);
-//     const data = await response.json();
-//     console.log(data);
-//   };
+  //   const makeQuery = async () => {
+  //     const response = await fetch("api/createPost", options);
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
 
   return (
     <>
       {/* <Header /> */}
       <div className="container">
-
-      <div className="text-black flex flex-col justify-start items-center min-h-screen bg-[#0C3BAA] p-[1em] w-full">
-        {/* TODO find exact blue and cream colors (maybe cream gradient?) desired) */}
-
-
-        {/* <form onSubmit={submitHandler} className="min-w-[16em] w-4/5 border-none "> */}
-        <form className="min-w-[16em] w-4/5 border-none ">
-          {/* <label htmlFor="title">Title</label> */}
-          <div className="bg-white h-[70vh]">
-            <input
-              type="text"
-              value={title}
-              name="title"
-              placeholder="Enter a title"
-              onChange={handleTitleChange}
-              required
-              className="mr-4"
-            />
-            <span className="pl-4 " >{title}</span>
-            <br/>
-            <br/>
-          <div className="bg-white ">
-            {/* https://quilljs.com/docs/api/ */}
-            <QuillNoSSRWrapper
-              modules={modules}
-              onChange={setContent}
-              theme="snow"
-              className=" min-h-[15vh] h-[60vh]"
-          
-          value={content}
-              >
-
-                
-              </QuillNoSSRWrapper>
-          
-
-          </div>
-          
-
-          </div>
-
-          
-          
-        </form>
-
-
-  <button className="p-2 m-4 fw-bold bg-blue-600 rounded" onClick={updateData}>Update</button>
-  :
-  <button href="/login" className="">
-    <Link href="/login">LOGIN TO POST AS YOURSELF</Link>
-    </button>
-
-
-{/* Temporary */}
+        <div className="text-black flex flex-col justify-start items-center min-h-screen bg-[#0C3BAA] p-[1em] w-full">
+          {/* TODO find exact blue and cream colors (maybe cream gradient?) desired) */}
+          {/* <form onSubmit={submitHandler} className="min-w-[16em] w-4/5 border-none "> */}
+          <form className="min-w-[16em] w-4/5 border-none ">
+            {/* <label htmlFor="title">Title</label> */}
+            <div className="bg-white h-[70vh]">
+              <input
+                type="text"
+                value={title}
+                name="title"
+                placeholder="Enter a title"
+                onChange={handleTitleChange}
+                required
+                className="mr-4"
+              />
+              <span className="pl-4 ">{title}</span>
+              <br />
+              <br />
+              <div className="bg-white ">
+                {/* https://quilljs.com/docs/api/ */}
+                <QuillNoSSRWrapper
+                  modules={modules}
+                  onChange={setContent}
+                  theme="snow"
+                  className=" min-h-[15vh] h-[60vh]"
+                  value={content}
+                ></QuillNoSSRWrapper>
+              </div>
+            </div>
+          </form>
+          <button
+            className="p-2 m-4 fw-bold bg-blue-600 rounded"
+            onClick={updateData}
+          >
+            Update
+          </button>
+          :
+          <button href="/login" className="">
+            <Link href="/login">LOGIN TO POST AS YOURSELF</Link>
+          </button>
+          {/* Temporary */}
           {/* <button className="p-2 m-4 fw-bold bg-blue-600 rounded ">Submit</button> */}
+        </div>
       </div>
-      </div>
-        <div className="container">
-
-        <details  > 
-      <summary className="fw-bold"> SHOW HTML VALUES FOR DATABASE</summary>
-
+      <div className="container">
+        <details>
+          <summary className="fw-bold"> SHOW HTML VALUES FOR DATABASE</summary>
 
           <p>Body: {content}</p>
-          
+
           <p>Title: {title}</p>
           <p>Subtitle: {subtitle}</p>
           <p>By: {author}</p>
-          <p>Created: {createddateTime}, Updated: {updateddateTime}</p>
-          
-          <p>URL Slug: {slugify(title.toLowerCase())}-{idNum}</p>
+          <p>
+            Created: {createddateTime}, Updated: {updateddateTime}
+          </p>
+
+          <p>URL Slug: {url}</p>
           {/* <p>Tags: {tagsArr}</p> */}
-          
+
           {/* <p>Comments: {commentsArr}</p> */}
-
-
         </details>
-        </div>
-
+      </div>
     </>
   );
 }
