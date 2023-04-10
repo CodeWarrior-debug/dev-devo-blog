c453ed8d76fa83ef19549d41f2e44adae43f4566
James Jordan <jpjordan1987@gmail.com>
2023-04-10 14:21:41 -0400

    using coder one video

---
+++ components/FilterForm.js
@@ -0,0 +1,18 @@
+// FilterForm.js
+
+import React from 'react';
+
+const FilterForm = ({ column }) => {
+    
+    // const { filterValue, setFilter } = column;
+    return (
+        <span>
+            <input
+                value={filterValue || ''}
+                onChange={(e) => setFilter(e.target.value)}
+            />
+        </span>
+    );
+};
+
+export default FilterForm;
--- pages/search-table.js
+++ pages/search-table.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-key */
 import React, { useEffect, useState, useMemo } from 'react'
-import blogsData from "../lib/blogsData.json"
-import { useGlobalFilter, useTable } from 'react-table'
-import "../styles/search-table.module.css"
+import { useTable, useFilters, useSortBy} from 'react-table'
+import '../styles/searchtable.module.css'
+// import FilterForm from '@/components/FilterForm'
 
 
 const ReactTable = () =>{
@@ -10,6 +10,7 @@ const ReactTable = () =>{
   const [data, setData] = useState([])
   // RESOURCE: https://github.com/machadop1407/react-table-tutorial
   // https://hygraph.com/blog/react-table
+  // https://www.youtube.com/watch?v=WRKEjPq75BY
 
   useEffect(()=>{
 
@@ -30,64 +31,83 @@ const columns = React.useMemo(
     {
       Header: "Title",
       accessor: "title",
+      // Filter: FilterForm,
     },
     {
       Header: "ID",
       accessor: "idNum",
+      // Filter: FilterForm,
     },
     {
       Header: "Date Updated",
       accessor: "updateddateTime",
+      // Filter: FilterForm,
     },
     {
       Header: "URL End",
       accessor: "url",
+      // Filter: FilterForm,
     },
     {
       Header: "Author",
       accessor: "author",
+      // Filter: FilterForm,
     },
     {
       Header: "Date Created",
       accessor: "createddateTime",
+      // Filter: FilterForm,
     },
   ],
   []
 );
 
-const { getTableProps, getTableBodyProps, headerGroups, rows, state, setGlobalFilter, prepareRow } =
-  useTable({ columns, data }, useGlobalFilter);
+// const { getTableProps, getTableBodyProps, headerGroups, rows, state, prepareRow } =
+//   useTable({ columns, data }, useFilters);
+const { getTableProps, getTableBodyProps, headerGroups, rows, state, prepareRow } =
+  useTable({ columns, data }, useSortBy);
+
+
+
 
 return (
-  <div className="search-table">
-    <div className="container">
-      <table {...getTableProps()}>
-        <thead>
+  <div>
+    {/* <FilterForm/> */}
+  <table className='searchTable' {...getTableProps()}>
+      <thead className='searchTable'>
           {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                  <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
-                ))}
+              <tr className='searchTable' {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                      <th  className='searchTable' {...column.getHeaderProps(column.getSortByToggleProps())}>
+                          {column.render('Header')}
+                          {column.isSorted ? (column.isSortedDesc ? "⬇️" : "⬆️") :""}
+                          <div>
+                            {/* {column.canFilter ? column.render('Filter'):"none"} */}
+                          </div>
+                      </th>
+                  ))}
               </tr>
-            );
+          ))}
+      </thead>
+      <tbody className='searchTable' {...getTableBodyProps()}>
+          {rows.map((row) => {
+              prepareRow(row);
+              return (
+                  <tr className='searchTable' {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                          return (
+                              <td  className='searchTable' {...cell.getCellProps()}>
+                                  {cell.render('Cell')}
+                              </td>
+                          );
+                      })}
+                  </tr>
+              );
           })}
-        </tbody>
-      </table>
-    </div>
-  </div>
+      </tbody>
+  </table>
+</div>
+
 );
 }
 
