b5967e2e8df472c5a0fbd8a100b0163fd27c64f1
James Jordan <jpjordan1987@gmail.com>
2023-04-21 12:47:14 -0400

    login page and compose correct

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,40 +1,26 @@
-import AuthDetails from '../components/AuthDetails'
-import Navbar from '../components/Navbar'
-import SignIn from '../components/SignIn'
-import SignUp from '../components/SignUp'
+import LoginButton from '../components/LoginButtton'
 import React from 'react'
-import {auth} from '../lib/firesStoreRef'
-import {GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
-
-export const GoogleSignInAPI = () => {
-  try {
-    let googleProvider = new GoogleAuthProvider();
-    let res = signInWithPopup(auth, googleProvider);
-    return res;
-  } catch (err) {
-    return err;
-  }
-};
-
-const Login = () => {
+import AccessToken from '@/components/AccessToken'
+import Navbar from '../components/Navbar'
 
 
+const NextAuth = () => {
   return (
     <>
     <Navbar/>
     <div className="container position-relative mt-5 pt-3">
         <div className="container position-relative pt-5">
-      
+          <div className='d-flex flex-row justify-content-center'>
 
+              <LoginButton/>
+              <AccessToken/>
 
-
-      <SignIn/>
-      <SignUp/>
-      <AuthDetails/>
+          </div>
       </div>
       </div>
+
     </>
   )
 }
 
-export default Login
+export default NextAuth
