6ad580bb1cc842480122e751b1860da0ea922a20
James Jordan <jpjordan1987@gmail.com>
2023-04-21 11:27:22 -0400

    app page setup no errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react' ;
+import {SessionProvider} from 'next-auth/react'
 
 
-export default function App({ Component, pageProps} : AppProps){
+export default function App({ Component, pageProps: {session, ...pageProps}} : AppProps){
   return (
   <>
+  <SessionProvider session={session}>
+
     <Component{ ...pageProps}/>
     <Analytics/>
+  </SessionProvider>
   </>
   )
 }
